bb44055de52aed942f69202a7e77abff69463e4a
Isaac Martinez <isaacmartinez2429@gmail.com>
2025-02-04 14:46:34 -0800

    Add project files.

---
+++ COMP003A.LectureActivity4-1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+// Author: Isaac Martinez
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: Calculate average grades using arrays and lists in C#
+*/
+namespace COMP003A.LectureActivity4_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
